aee11fdbb1ec30e50f724bd5b44c29a12176fea7
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-10-17 10:46:53 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -3,23 +3,23 @@ const assert = require('assert');
 
 eval(fs.readFileSync('code.js')+'');
 
-var graph = {
+var graph1 = {
   A: ['B'],
   B: ['A', 'C'],
   C: ['D'],
   D: []
 };
-assert(JSON.stringify(hasCycle(graph)) == JSON.stringify(false));
+assert(JSON.stringify(hasCycle(graph1)) == JSON.stringify(false));
 
-var graph = {
+var graph2 = {
     A: ['B'],
     B: [],
     C: ['D'],
     D: []
 };
-assert(JSON.stringify(hasCycle(graph)) == JSON.stringify(false));
+assert(JSON.stringify(hasCycle(graph2)) == JSON.stringify(false));
 
-var graph = {
+var graph3 = {
     A: ['B', 'C'],
     B: ['D', 'E'],
     C: ['F', 'G'],
@@ -29,14 +29,14 @@ var graph = {
     G: ['A'],
     H: []
 };
-assert(JSON.stringify(hasCycle(graph)) == JSON.stringify(true));
+assert(JSON.stringify(hasCycle(graph3)) == JSON.stringify(true));
 
-var graph = {
+var graph4 = {
     A: ['B', 'C'],
     B: ['C'],
     C: ['A'],
 };
-assert(JSON.stringify(hasCycle(graph)) == JSON.stringify(true));
+assert(JSON.stringify(hasCycle(graph4)) == JSON.stringify(true));
 
-var graph = { };
-assert(JSON.stringify(hasCycle(graph)) == JSON.stringify(false));
+var graph5 = { };
+assert(JSON.stringify(hasCycle(graph5)) == JSON.stringify(false));
